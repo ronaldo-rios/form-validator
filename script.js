d6014dc7cf4474a40e6f66e1d83e1a0b4d959dd6
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-18 13:40:17 -0300

    end project

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ let validator = {
 
         let inputs = form.querySlectorAll('input')
         validator.clearErrors()
-        for(let i = 0; i < inputs.lenght; i++){
+       
+        for(let i = 0; i < inputs.length; i++){
             let input = inputs[i]
             let check = validator.checkInput(input)
 
             if(check !== true){
                 send = false
                 validator.showError(input, check)
-            }
-            
+            }   
         }
-
+        send = false;
         if(send){
             form.submit()
         }
@@ -27,18 +27,28 @@ let validator = {
                 rules = rules.split('|')
                 for(let e in rules){
                     let rDetails = rules[e].split('=')
+
                     switch(rDetails[0]){
+
                         case 'required':
                             if(input.value == ''){
                                 return 'Campo obrigatório'
                             }
-                        break;
+                             break;
+
+                        case 'min':
+                            if(input.value.length < rDetails[1]){
+                                return `Campo deve ter pelo menos ${rDetails} caracteres`                       
+                        }
+                             break;
                     }
                 }
             }
 
             return true
         }
+
+
         //Show error é uma função criada para exibir o erro e em qual input
         showError:(input, error)=> {
             input.style.borderColor = '#FF0000'
@@ -48,17 +58,22 @@ let validator = {
             errorElement.innerHTML = error
             input.parentElement.insertBefore(errorElement, input.ElementSibling)
         }
-        clearErrors:() => {
+
+
+        clearError:() => {
             let inputs = document.querySelectorAll('input')
-            for(let i =0; i < inputs.lenght; i++){
+            for(let i = 0; i < inputs.length; i++){
                 inputs[i].style = ''
             }
             let errorElements = document.querySelectorAll('.error')
-        for(let i = 0; i < errorElements.lenght; i++){
+            for(let i = 0; i < errorElements.length; i++){
             errorElements[i].remove()
         }
     }
     }
 }
+
+
+
 let form = document.querySelector('.validator')
 form.addEventListener('submit', validator.handleSubmit)
