5d0bb7d70cdcac6b63ef267b61d73d1e9710499f
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-18 23:47:35 -0300

    Final commit

--- script.js
+++ script.js
@@ -1,79 +1,84 @@
 
 let validator = {
-    handleSubmit:(event)=> {
+    handleSubmit: (event) => {
         event.preventDefault()
         let send = true
 
         let inputs = form.querySlectorAll('input')
         validator.clearErrors()
-       
-        for(let i = 0; i < inputs.length; i++){
+
+        for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i]
             let check = validator.checkInput(input)
 
-            if(check !== true){
+            if (check !== true) {
                 send = false
                 validator.showError(input, check)
-            }   
+            }
         }
-        send = false;
-        if(send){
+
+        if (send) {
             form.submit()
         }
-        checkInput:(input) => {
-            let rules = input.getAttribute('data-rules')
+    },
         
-            if(rules !== null){
-                rules = rules.split('|')
-                for(let e in rules){
-                    let rDetails = rules[e].split('=')
+    
+    // regras de campo obrigatório e required
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules')
 
-                    switch(rDetails[0]){
+        if (rules !== null) {
+            rules = rules.split('|')
+            for (let e in rules) {
+                let rDetails = rules[e].split('=')
 
-                        case 'required':
-                            if(input.value == ''){
-                                return 'Campo obrigatório'
-                            }
-                             break;
-
-                        case 'min':
-                            if(input.value.length < rDetails[1]){
-                                return `Campo deve ter pelo menos ${rDetails} caracteres`                       
+                switch (rDetails[0]) {
+                    case 'required':
+                        if (input.value == '') {
+                            return 'Campo é obrigatório'
                         }
-                             break;
-                    }
+                        break;
+
+                    case 'min':
+                        if(input.value.length < rDetails[1]){
+                            return 'O campo tem que ter pelo menos ' + rDetails + ' caracteres'
+                        }
+                        break;
                 }
             }
-
-            return true
         }
-
+        return true
+    },
 
         //Show error é uma função criada para exibir o erro e em qual input
-        showError:(input, error)=> {
-            input.style.borderColor = '#FF0000'
+    showError: (input, error) => {
+    input.style.borderColor = '#FF0000'
 
-            let errorElement = document.createElement('div')
-            errorElement.classList.add('error')
-            errorElement.innerHTML = error
-            input.parentElement.insertBefore(errorElement, input.ElementSibling)
-        }
+    let errorElement = document.createElement('div')
+    errorElement.classList.add('error')
+    errorElement.innerHTML = error
+    input.parentElement.insertBefore(errorElement, input.ElementSibling)
+    },
 
-
-        clearError:() => {
-            let inputs = document.querySelectorAll('input')
-            for(let i = 0; i < inputs.length; i++){
-                inputs[i].style = ''
+    // Limpar as propriedades que já aparecem
+    clearErrors:() => {
+        let inputs = document.querySelectorAll('input')
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].style = ''
             }
-            let errorElements = document.querySelectorAll('.error')
-            for(let i = 0; i < errorElements.length; i++){
+        let errorElements = document.querySelectorAll('.error')
+        for(let i = 0; i < errorElements.length; i++){
             errorElements[i].remove()
         }
-    }
-    }
+    }    
+
 }
+
 
 
 
 let form = document.querySelector('.validator')
 form.addEventListener('submit', validator.handleSubmit)
+
+
+
